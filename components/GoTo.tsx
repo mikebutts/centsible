e211800f62e725b6f4a4c30f4d5b3142db34ffba
Mike Butts <mgbutts@gmail.com>
2025-05-14 15:02:51 -0400

    added lgout

--- components/GoTo.tsx
+++ components/GoTo.tsx
@@ -1,34 +1,31 @@
-"use client"
+'use client'
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useAuth } from "@/context/AuthContext"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
+export default function GoTo() {
+    const { currentUser, logout } = useAuth()
 
-export default function GoTo(){
+    const isAuthenticated = !!currentUser
 
-    const isAuthenticated = false;
-    const path = usePathname();
+    const path = usePathname()
 
-    return(
-        <div className="goto"> 
-    { path == '/' && (
-        <> 
-            <Link href={'/dashboard?register=true'}>
-                <p> Sign Up</p>         
-            </Link>
-            <Link href={'/dashboard'}>
-                <button>Login &rarr;</button>
-            
-            </Link>
-        </>
-    )}
-        {(isAuthenticated && path == '/dashboard') && (
-
-            <button>Log Out</button>
-
-        )}
-        
-
+    return (
+        <div className="goto">
+            {path == '/' && (
+                <>
+                    <Link href={'/dashboard?register=true'}>
+                        <p>Sign Up</p>
+                    </Link>
+                    <Link href={'/dashboard'}>
+                        <button>Login &rarr;</button>
+                    </Link>
+                </>
+            )}
+            {(isAuthenticated && path == '/dashboard') && (
+                <button onClick={logout}>Logout</button>
+            )}
         </div>
     )
 }
